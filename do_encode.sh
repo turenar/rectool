8edc7f8e484391cf8f60f07aecd30a701a9da28a
Turenar <snswinhaiku.lo@gmail.com>
2013-07-15 18:29:42 +0900

    update do_encode.sh

--- do_encode.sh
+++ do_encode.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 FROM=/data/epgrec
 TO=/data/encoded
+ARCHIVE=/data/archives
+export DISPLAY=:0
 
 
 EPGREC_D="$(dirname "$0")"
@@ -9,16 +11,15 @@ BASEDIR="$(dirname "$(readlink -f "$0")")"
 export AVCONV_DATADIR="${BASEDIR}"
 # avconv's loglevel
 loglevel=${av_loglevel:-error}
-#echo "BASEDIR: $BASEDIR"
-#echo "OUTPUT: $OUTPUT"
 
+FNFULLPATH="$(readlink -f "${OUTPUT}")"
 FILENAME="$(basename "$OUTPUT")"
 FN_NOSUF="${FILENAME%.*}"
 
 echo "FILENAME: $FILENAME"
 #echo "FN_NOSUF: $FN_NOSUF"
 
-cd "${FROM}"
+cd "$(dirname "$OUTPUT")"
 
 if [ ! -e "${FILENAME}" ]; then
 	echo "file is not found. abort."
@@ -29,50 +30,64 @@ fullfile="${TO}/full/${FN_NOSUF}-full.mp4"
 minifile="${TO}/mini/${FN_NOSUF}-mini.mp4"
 
 if [ ! -v 'av_encskip' ]; then
+	exec 9>>/var/lock/epgrec.encode
+	flock -n 9
+	if [ $? -ne 0 ]; then
+		echo 'Waiting for exclusive lock...'
+		flock 9
+	fi
+
 	tempfile="$(mktemp -d /tmp/jobs.encode.XXXXXXXXXX)"
 	if [ $? -ne 0 -o -z "${tempfile}" ]; then
 		echo "failed mktemp"
 		exit 1
 	fi
+	av_encfile="${OUTPUT}"
+	echo "Trying copying source file"
+	if nice -n 19 ionice -c 3 cp "${OUTPUT}" "${tempfile}/source.ts"; then
+		av_encfile="${tempfile}/source.ts"
+	else
+		rm -f "${tempfile}/source.ts"
+	fi
 
 	# check source.ts
-	avconv -i "${FILENAME}" > "${tempfile}/tsstat" 2>&1
+	avconv -i "${av_encfile}" > "${tempfile}/tsstat" 2>&1
 
-	video_map=$(grep 'Video: mpeg2video' < "${tempfile}/tsstat" \
-		| sed -e 's/^.\+Stream #0.\([0-9]\+\).\+$/0:\1/' \
-		| sort -u )
-	audio_map=$(grep 'Audio: aac' < "${tempfile}/tsstat" \
+	video_map="$(grep 'Video: mpeg2video' < "${tempfile}/tsstat" \
+		| sed -e 's/^.\+Stream #0.\([0-9]\+\).\+$/-map 0:\1/' \
+		| sort -u | tr '\n' ' ')"
+	audio_map="$(grep 'Audio: aac' < "${tempfile}/tsstat" \
 		| grep -P '\d{3} kb/s' \
-		| sed -e 's/^.\+Stream #0.\([0-9]\+\).\+$/0:\1/' \
-		| sort -u )
+		| sed -e 's/^.\+Stream #0.\([0-9]\+\).\+$/-map 0:\1/' \
+		| sort -u | tr '\n' ' ')"
 
 	echo "video.map: ${video_map}"
 	echo "audio.map: ${audio_map}"
 
 
 	echo "avconv with full"
-	nice -n 19 avconv -i "${OUTPUT}" -loglevel ${loglevel} -y -f mp4 \
+	nice -n 19 avconv -i "${av_encfile}" -loglevel ${loglevel} -y -f mp4 \
 		-pre:0 hq -vcodec libx264 -vsync 1 \
 		-me_method umh -bufsize 20000k -maxrate 16000k \
 		-r 30000/1001 -deinterlace -aspect 16:9 \
 		-ab 256k -crf 20 -ss 00:00:01 \
-		-map ${video_map} -map ${audio_map} -acodec libfaac \
-		"${tempfile}/full.mp4"
+		${video_map} ${audio_map} -acodec libfaac \
+		"${tempfile}/full.mp4" || exit 1
 	echo "avconv with mini"
-	nice -n 19 avconv -i "${OUTPUT}" -loglevel ${loglevel} -y -f mp4 \
+	nice -n 19 avconv -i "${av_encfile}" -loglevel ${loglevel} -y -f mp4 \
 		-pre:0 slow_firstpass -vcodec libx264 -vsync 1 -threads 3 \
 		-pass 1 -passlogfile ${tempfile}/passlog -qcomp 0.8 -me_method dia \
 		-r 30000/1001 -deinterlace -aspect 16:9 -s 640x480 \
 		-vb 900k -ab 160k -ss 00:00:01 \
-		-map ${video_map} -map ${audio_map} -acodec libfaac \
-		/dev/null
-	nice -n 19 avconv -i "${OUTPUT}" -loglevel ${loglevel} -y -f mp4 \
+		${video_map} ${audio_map} -acodec libfaac \
+		/dev/null || exit 1
+	nice -n 19 avconv -i "${av_encfile}" -loglevel ${loglevel} -y -f mp4 \
 		-pre:0 slow -vcodec libx264 -vsync 1 -threads 3 \
 		-pass 2 -passlogfile ${tempfile}/passlog -qcomp 0.8 -me_method umh \
 		-r 30000/1001 -deinterlace -aspect 16:9 -s 640x480 \
 		-vb 900k -ab 160k -ss 00:00:01 \
-		-map ${video_map} -map ${audio_map} -acodec libfaac \
-		"${tempfile}/mini.mp4"
+		${video_map} ${audio_map} -acodec libfaac \
+		"${tempfile}/mini.mp4" || exit 1
 	qt-faststart "${tempfile}/full.mp4" \
 		"${fullfile}"
 	qt-faststart "${tempfile}/mini.mp4" \
@@ -85,31 +100,40 @@ if [ ! -v 'av_encskip' ]; then
 fi
 
 if [ -e "${fullfile}" ]; then
-	until php "${EPGREC_D}/mediatomb-update.php" "${FROM}/${FILENAME}" "${fullfile}"; do
+	until php "${EPGREC_D}/mediatomb-update.php" "${FNFULLPATH}" "${fullfile}"; do
 		echo -n 'Wait..'
 		sleep 1
 	done
 fi
 if [ -e "${minifile}" ]; then
-	until php "${EPGREC_D}/mediatomb-update.php" "${FROM}/${FILENAME}" "${minifile}" " (low)"; do
+	until php "${EPGREC_D}/mediatomb-update.php" "${FNFULLPATH}" "${minifile}" ' (low)'; do
 		echo -n 'Wait..'
 		sleep 1
 	done
 fi
 
 cd "${TO}/full"
-"${BASEDIR}/update.sh" "${FN_NOSUF}-full.mp4"
+"${EPGREC_D}/update-filepath.sh" "${FN_NOSUF}-full.mp4"
 
-if [ -e */"${FN_NOSUF}-full.mp4" ]; then
+if [ -e "${FN_NOSUF}-full.mp4" ]; then
+	php "${EPGREC_D}/epgrec-update.php" "${FILENAME}" "mp4/${FN_NOSUF}-full.mp4"
+	[ -e "${EPGREC_D}/thumbs/${FILENAME}.jpg" ] && mv "${EPGREC_D}/thumbs/${FILENAME}.jpg" "${EPGREC_D}/thumbs/${FN_NOSUF}-full.mp4.jpg"
+elif [ -e */"${FN_NOSUF}-full.mp4" ]; then
 	_filename=*/"${FN_NOSUF}-full.mp4"
 	php "${EPGREC_D}/epgrec-update.php" "${FILENAME}" "mp4/${_filename}"
-	[ -e "${EPGREC_D}/thumbs/${FILENAME}.jpg" ] && mv "${EPGREC_D}/thumbs/${FILENAME}.jpg" "${EPGREC_D}/thumbs/${FN_NOSUF}-full.mp4.jpg"
-elif [ -e "${FN_NOSUF}-full.mp4" ]; then
-	php "${EPGREC_D}/epgrec-update.php" "${FILENAME}" "mp4/${FN_NOSUF}-full.mp4"
 	[ -e "${EPGREC_D}/thumbs/${FILENAME}.jpg" ] && mv "${EPGREC_D}/thumbs/${FILENAME}.jpg" "${EPGREC_D}/thumbs/${FN_NOSUF}-full.mp4.jpg"
 fi
 
 cd "${TO}/mini"
-"${BASEDIR}/update.sh" "${FN_NOSUF}-mini.mp4"
+"${EPGREC_D}/update-filepath.sh" "${FN_NOSUF}-mini.mp4"
+
+if [ ! -v 'av_encskip' -a -e "${FROM}/${FILENAME}" ]; then
+	echo 'Moving file into archives'
+	cd "${FROM}"
+	ln "${FILENAME}" "${ARCHIVE}/${FILENAME}"
+	sleep 1
+	php "${EPGREC_D}/mediatomb-update.php" "${FNFULLPATH}" "${ARCHIVE}/${FILENAME}"
+	rm "${FILENAME}"
+fi
 
 #rm -f ${FILENAME} ${tempfile}
