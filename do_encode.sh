549ddb6aeb79b07012729b734b10f50f56ff39f3
Turenar <snswinhaiku.lo@gmail.com>
2013-10-06 16:21:10 +0900

    Remove low-quality encoding
    Buffer with dd: no tmpfs is needed
      dd reads with larger packets, avoiding too frequent hdd accessing
      you can change buffer size (ibs=~~) DO NOT CHANGE if=... section
    
    env av_map_skip: Do not specify -map
      This is workaround for libav-HEAD.
      executing avconv with or without output file
        cause mis-numbering stream mapping
          (ex. 'avconv -i pipe: -map 0:0' encodes video stream
               'avconv -i pipe: -map 0:0 output' tries to encode data stream)
      'av_map_skip= ./do_encode.sh' will not specify any stream mappings
        (but double audio stream may not be saved in output)

--- do_encode.sh
+++ do_encode.sh
@@ -31,7 +31,7 @@ if [ ! -e "${FILENAME}" ]; then
 fi
 
 fullfile="${TO}/full/${FN_NOSUF}-full.mp4"
-minifile="${TO}/mini/${FN_NOSUF}-mini.mp4"
+#minifile="${TO}/mini/${FN_NOSUF}-mini.mp4"
 
 if [ ! -v 'av_encskip' ]; then
 	exec 9>>/var/lock/epgrec.encode
@@ -47,16 +47,11 @@ if [ ! -v 'av_encskip' ]; then
 		exit 1
 	fi
 	av_encfile="${OUTPUT}"
-	echo "Trying copying source file"
-	if nice -n 19 ionice -c 3 cp "${OUTPUT}" "${tempfile}/source.ts"; then
-		av_encfile="${tempfile}/source.ts"
-	else
-		rm -f "${tempfile}/source.ts"
-	fi
 
 	# check source.ts
-	avconv -i "${av_encfile}" > "${tempfile}/tsstat" 2>&1
-
+	avconv -i "${OUTPUT}" > "${tempfile}/tsstat" 2>&1
+	
+	if [ ! -v "av_map_skip" ]; then
 	video_map="$(grep 'Video: mpeg2video' < "${tempfile}/tsstat" \
 		| sed -e 's/^.\+Stream #0.\([0-9]\+\).\+$/-map 0:\1/' \
 		| sort -u | tr '\n' ' ')"
@@ -64,39 +59,38 @@ if [ ! -v 'av_encskip' ]; then
 		| grep -P '\d{3} kb/s' \
 		| sed -e 's/^.\+Stream #0.\([0-9]\+\).\+$/-map 0:\1/' \
 		| sort -u | tr '\n' ' ')"
+	fi
 
 	echo "video.map: ${video_map}"
 	echo "audio.map: ${audio_map}"
 
-	if [ ! -v "av_fullskip" ]; then
-		echo "avconv with full"
-		nice -n 19 avconv -i "${av_encfile}" -loglevel ${loglevel} -y -f mp4 \
-			-pre:0 hq -vcodec libx264 -vsync 1 \
-			-acodec copy -bufsize 20000k -maxrate 16000k \
-			-r 30000/1001 -filter:v yadif -aspect 16:9 \
-			-crf 22 -ss 00:00:01 \
-			${video_map} ${audio_map} \
-			"${tempfile}/full.mp4" || exit 1
-	fi
-	echo "avconv with mini"
-	nice -n 19 avconv -i "${av_encfile}" -loglevel ${loglevel} -y -f mp4 \
-		-pre:0 slow_firstpass -vcodec libx264 -vsync 1 \
-		-pass 1 -passlogfile ${tempfile}/passlog -qcomp 0.8 \
-		-r 30000/1001 -filter:v yadif -aspect 16:9 -s 640x480 \
-		-vb 750k -an -ss 00:00:01 \
-		${video_map} \
-		/dev/null || exit 1
-	nice -n 19 avconv -i "${av_encfile}" -loglevel ${loglevel} -y -f mp4 \
-		-pre:0 slow -vcodec libx264 -vsync 1 \
-		-pass 2 -passlogfile ${tempfile}/passlog -qcomp 0.8 \
-		-r 30000/1001 -filter:v yadif -aspect 16:9 -s 640x480 \
-		-vb 750k -ab 160k -ss 00:00:01 -mixed-refs 0 \
-		${video_map} ${audio_map} -acodec libfaac \
-		"${tempfile}/mini.mp4" || exit 1
-	[ ! -v "av_fullskip" ] && qt-faststart "${tempfile}/full.mp4" \
+	echo "avconv with full"
+	dd if="${OUTPUT}" ibs=512M obs=64k | avconv -i pipe: -loglevel ${loglevel} -y -f mp4 \
+		-pre:0 hq -vcodec libx264 -vsync 1 \
+		-acodec copy -bufsize 20000k -maxrate 16000k \
+		-r 30000/1001 -filter:v yadif -aspect 16:9 \
+		-crf 20 -ss 00:00:01 \
+		${video_map} ${audio_map} \
+		"${tempfile}/full.mp4" || exit 1
+	#echo "avconv with mini"
+	#nice -n 19 avconv -i "${av_encfile}" -loglevel ${loglevel} -y -f mp4 \
+	#	-pre:0 slow_firstpass -vcodec libx264 -vsync 1 \
+	#	-pass 1 -passlogfile ${tempfile}/passlog -qcomp 0.8 \
+	#	-r 30000/1001 -filter:v yadif -aspect 16:9 -s 640x480 \
+	#	-vb 750k -an -ss 00:00:01 \
+	#	${video_map} \
+	#	/dev/null || exit 1
+	#nice -n 19 avconv -i "${av_encfile}" -loglevel ${loglevel} -y -f mp4 \
+	#	-pre:0 slow -vcodec libx264 -vsync 1 \
+	#	-pass 2 -passlogfile ${tempfile}/passlog -qcomp 0.8 \
+	#	-r 30000/1001 -filter:v yadif -aspect 16:9 -s 640x480 \
+	#	-vb 750k -ab 160k -ss 00:00:01 -mixed-refs 0 \
+	#	${video_map} ${audio_map} -acodec libfaac \
+	#	"${tempfile}/mini.mp4" || exit 1
+	qt-faststart "${tempfile}/full.mp4" \
 		"${fullfile}"
-	qt-faststart "${tempfile}/mini.mp4" \
-		"${minifile}"
+	#qt-faststart "${tempfile}/mini.mp4" \
+	#	"${minifile}"
 	echo "finish! cleaning up..."
 	rm ${tempfile}/*
 	rmdir ${tempfile}
@@ -104,18 +98,18 @@ if [ ! -v 'av_encskip' ]; then
 	sleep 1
 fi
 
-if [ ! -v "av_fullskip" -a -e "${fullfile}" ]; then
+if [ -e "${fullfile}" ]; then
 	until php "${EPGREC_D}/mediatomb-update.php" "${FNFULLPATH}" "${fullfile}"; do
 		echo -n 'Wait..'
 		sleep 1
 	done
 fi
-if [ -e "${minifile}" ]; then
-	until php "${EPGREC_D}/mediatomb-update.php" "${FNFULLPATH}" "${minifile}" ' (low)'; do
-		echo -n 'Wait..'
-		sleep 1
-	done
-fi
+#if [ -e "${minifile}" ]; then
+#	until php "${EPGREC_D}/mediatomb-update.php" "${FNFULLPATH}" "${minifile}" ' (low)'; do
+#		echo -n 'Wait..'
+#		sleep 1
+#	done
+#fi
 
 cd "${TO}/full"
 "${EPGREC_D}/update-filepath.sh" "${FN_NOSUF}-full.mp4"
@@ -123,14 +117,16 @@ cd "${TO}/full"
 if [ -e "${FN_NOSUF}-full.mp4" ]; then
 	php "${EPGREC_D}/epgrec-update.php" "${FILENAME}" "mp4/${FN_NOSUF}-full.mp4"
 	[ -e "${EPGREC_D}/thumbs/${FILENAME}.jpg" ] && mv "${EPGREC_D}/thumbs/${FILENAME}.jpg" "${EPGREC_D}/thumbs/${FN_NOSUF}-full.mp4.jpg"
+	echo "${TO}/${FN_NOSUF}" > "${TO}/mini/${FN_NOSUF}-mini.enc"
 elif [ -e */"${FN_NOSUF}-full.mp4" ]; then
 	_filename=*/"${FN_NOSUF}-full.mp4"
+	echo "${TO}/${_filename}" > "${TO}/mini/${FN_NOSUF}-mini.enc"
 	php "${EPGREC_D}/epgrec-update.php" "${FILENAME}" "mp4/${_filename}"
 	[ -e "${EPGREC_D}/thumbs/${FILENAME}.jpg" ] && mv "${EPGREC_D}/thumbs/${FILENAME}.jpg" "${EPGREC_D}/thumbs/${FN_NOSUF}-full.mp4.jpg"
 fi
 
 cd "${TO}/mini"
-"${EPGREC_D}/update-filepath.sh" "${FN_NOSUF}-mini.mp4"
+"${EPGREC_D}/update-filepath.sh" "${FN_NOSUF}-mini.enc"
 
 if [ ! -v 'av_encskip' -a -e "${FROM}/${FILENAME}" ]; then
 	echo 'Moving file into archives'
