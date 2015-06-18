02208f2c14925469d1d7b92f987533aa03185dc3
Turenar <miki@turenar.xyz>
2015-06-18 20:37:43 +0900

    syobocal: add cache

--- syobocal_rename.php
+++ syobocal_rename.php
@@ -15,13 +15,18 @@ class SyobocalRenamer {
 	private $application_name;
 	private $cfg;
 	private $log_level = self::INFO;
+	private $cache_db;
+
 	private $flag_epgrec = false;
 	private $flag_no_action = false;
 	private $epgrec_config = null;
 	private $epgrec_dbconn = null;
 
 	function __construct($cfg){
+		global $script_path;
 		$this->cfg = $cfg;
+
+		$this->cache_db = new SQLite3($script_path . '/syobocal_cache.db');
 	}
 
 	function run($argv){
@@ -39,6 +44,8 @@ class SyobocalRenamer {
 			$arg = array_shift($argv);
 			if($arg === null){
 				break;
+			} elseif ($arg == '-d' || $arg == '--debug') {
+				$this->log_level = self::DEBUG;
 			} elseif ($arg == '-n' || $arg == '--get-path') {
 				$no_action = true;
 			} elseif ($arg == '-e' || $arg == '--epgrec') {
@@ -70,7 +77,10 @@ class SyobocalRenamer {
 		$this->flag_epgrec = $epgrec;
 		$this->flag_no_action = $no_action;
 
+		$this->update_db();
+
 		foreach ($file_path as $elem) {
+			$this->_dbg("target: %s", $elem);
 			$this->process($elem);
 		}
 	}
@@ -102,10 +112,8 @@ class SyobocalRenamer {
 		$start_date = DateTime::createFromFormat($cfg['date_format'], $matches['date']);
 		//$start_date->modify('-15 min');
 		date_modify($start_date, '-15 min');
-		$start_date = $start_date->format('YmdHi');
 		$end_date = DateTime::createFromFormat($cfg['date_format'], $matches['date']);
 		date_modify($end_date, '+75 min');
-		$end_date = $end_date->format('YmdHi');
 		$title = $matches['title'];
 		$extension = $matches['extension'];
 
@@ -135,35 +143,81 @@ class SyobocalRenamer {
 	}
 
 	function search_program($start_date, $end_date, $channel, $title) {
-		$url = sprintf("http://cal.syoboi.jp/rss2.php?start=%s&end=%s&usr=%s&alt=json",
-			$start_date, $end_date, urlencode($this->cfg['user']));
-		$json = json_decode(file_get_contents($url), true);
-
+		$normTitle = mb_substr(Normalizer::normalize($title, Normalizer::FORM_KC), 0, 5);
 		$channelmap = $this->cfg['channel'];
 
+		$stmt = $this->cache_db->prepare(
+			'SELECT subtitle AS SubTitle, channel AS ChName, title_tbl.title_id AS TID, title AS Title, season AS Season, count AS Count
+			FROM program_tbl LEFT JOIN title_tbl ON title_tbl.title_id = program_tbl.title_id
+			WHERE start_date >= :start_date AND end_date <= :end_date');
+		$stTimestamp = $start_date->getTimestamp();
+		$edTimestamp = $end_date->getTimestamp();
+		$stmt->bindParam('start_date', $stTimestamp, SQLITE3_INTEGER);
+		$stmt->bindParam('end_date', $edTimestamp, SQLITE3_INTEGER);
+		$result = $stmt->execute();
+		if ($program = $result->fetchArray(SQLITE3_ASSOC)) {
+			$found = null;
+			$found_without_channel = array();
+			do {
+				$progChId = isset($channelmap[$program['ChName']]) ? $channelmap[$program['ChName']] : null;
+				$progTitle = Normalizer::normalize($program['Title'], Normalizer::FORM_KC);
+				if (strpos($progTitle, $normTitle) !== false) {
+					if($progChId == $channel){
+						return $program;
+					} else {
+						$found_without_channel[] = $program;
+					}
+				}
+			} while ($program = $result->fetchArray(SQLITE3_ASSOC));
+		}
+
+		// cache miss
+		$url = sprintf("http://cal.syoboi.jp/rss2.php?start=%s&end=%s&usr=%s&alt=json",
+			$start_date->format('YmdHi'), $end_date->format('YmdHi'), urlencode($this->cfg['user']));
+		$json = json_decode(file_get_contents($url), true);
+
+		$this->_dbg(' program cache miss: %s', $title);
 		$found = null;
-		$found_without_channel = null;
+		$found_without_channel = array();
 		foreach ($json['items'] as $program) {
+			$stmt = $this->cache_db->prepare('INSERT OR REPLACE
+				INTO program_tbl(program_id, start_date, end_date, title_id, channel, subtitle, count)
+				VALUES (:program_id, :start_date, :end_date, :title_id, :channel, :subtitle, :count)');
+			$stmt->bindParam('program_id', $program['PID']);
+			$stmt->bindParam('start_date', $program['StTime']);
+			$stmt->bindParam('end_date', $program['EdTime']);
+			$stmt->bindParam('title_id', $program['TID']);
+			$stmt->bindParam('channel', $program['ChName']);
+			$stmt->bindParam('subtitle', $program['SubTitle']);
+			$stmt->bindParam('count', $program['Count']);
+			$stmt->execute();
+			$stmt = $this->cache_db->prepare('INSERT OR REPLACE
+				INTO title_tbl(title_id, title, season)
+				VALUES (:title_id, :title, (SELECT season FROM title_tbl WHERE title_id = :title_id))');
+			$stmt->bindParam('title_id', $program['TID']);
+			$stmt->bindParam('title', $program['Title']);
+			$stmt->execute();
+
 			$progChId = isset($channelmap[$program['ChName']]) ? $channelmap[$program['ChName']] : null;
 			$progTitle = Normalizer::normalize($program['Title'], Normalizer::FORM_KC);
-			$normTitle = mb_substr(Normalizer::normalize($title, Normalizer::FORM_KC), 0, 5);
 			if (strpos($progTitle, $normTitle) !== false) {
 				if($progChId == $channel){
 					$found = $program;
-					break;
 				} else {
-					$found_without_channel = $program;
+					$found_without_channel[] = $program;
 				}
 			}
 		}
 
 		if ($found === null) {
-			if ($found_without_channel === null) {
+			if (count($found_without_channel) === 0) {
 				$this->_err("Specified program is not found. title=%s", $title);
 			} else {
 				$this->_err("Specified named program seems to be found, but channel is not matched.");
 				$this->_err(" title=%s, channel=%s", $title, $channel);
-				$this->_err(" progTitle=%s, progChName=%s", $program['Title'], $program['ChName']);
+				foreach ($found_without_channel as $program) {
+					$this->_err(" progTitle=%s, progChName=%s", $program['Title'], $program['ChName']);
+				}
 				$this->_err("Check your syobocal_channel.json");
 			}
 			exit(ERR_FATAL_CONFIG);
@@ -171,8 +225,21 @@ class SyobocalRenamer {
 		return $found;
 	}
 
-	function search_season($titleId) {
-		$url = "http://cal.syoboi.jp/db.php?Command=TitleLookup&TID=$titleId";
+	function search_season($title_id) {
+		$stmt = $this->cache_db->prepare('SELECT season FROM title_tbl WHERE title_id = :title_id');
+		$stmt->bindParam('title_id', $title_id, SQLITE3_INTEGER);
+		$result = $stmt->execute();
+		if ($res_arr = $result->fetchArray(SQLITE3_NUM)) {
+			$found_row = true;
+			if($res_arr[0] !== null){
+				return $res_arr[0];
+			}
+		} else {
+			$found_row = false;
+		}
+
+		$this->_dbg(' title cache miss: %d', $title_id);
+		$url = "http://cal.syoboi.jp/db.php?Command=TitleLookup&TID=$title_id";
 
 		$title_data = simplexml_load_string(file_get_contents($url));
 		$title_data = $title_data->TitleItems->TitleItem;
@@ -180,7 +247,15 @@ class SyobocalRenamer {
 		if (empty($title_data->FirstYear) || empty($title_data->FirstMonth)) {
 			return "unknown";
 		} else {
-			return self::get_season($title_data->FirstYear, $title_data->FirstMonth);
+			$season = self::get_season($title_data->FirstYear, $title_data->FirstMonth);
+			if ($found_row) {
+				$stmt = $this->cache_db->prepare('UPDATE title_tbl SET season = :season WHERE title_id = :title_id');
+			} else {
+				$stmt = $this->cache_db->prepare('INSERT INTO title_tbl (title_id, season) VALUES (:title_id, :season)');
+			}
+			$stmt->bindParam('title_id', $title_id, SQLITE3_INTEGER);
+			$stmt->bindParam('season', $season, SQLITE3_TEXT);
+			$result = $stmt->execute();
 		}
 	}
 
@@ -188,7 +263,7 @@ class SyobocalRenamer {
 		$dstdir = dirname($dst);
 
 		if(!file_exists($src)){
-			$this->_err("source file is not exist: %d", $src);
+			$this->_err("source file is not exist: %s", $src);
 			return false;
 		}
 		if(!is_dir($dstdir)) {
@@ -312,6 +387,39 @@ class SyobocalRenamer {
 	function _dbg($message) {
 		$format = func_get_args();
 		$this->_log(self::DEBUG, 'D', $format);
+	}
+
+	function update_db() {
+		$cache_db = $this->cache_db;
+		$stmt = $cache_db->prepare("SELECT name FROM sqlite_master WHERE type='table' AND name = 'config_tbl'");
+		$dbver = 0;
+		$result = $stmt->execute();
+		if ($result->fetchArray(SQLITE3_NUM)) {
+			$this->_dbg('config_tbl is found');
+			$stmt = $cache_db->prepare("SELECT value FROM config_tbl WHERE key = 'dbver'");
+			$result = $stmt->execute();
+			if ($res_arr = $result->fetchArray(SQLITE3_NUM)) {
+				$dbver = $res_arr[0];
+				$this->_dbg('dbver is found: %d', $dbver);
+			}
+		}
+
+		if ($dbver <= 0) {
+			$this->_dbg('Creating cache database...');
+			$this->_sqlite_query('CREATE TABLE config_tbl(key TEXT, value INT)');
+			$this->_sqlite_query('CREATE TABLE program_tbl (
+				program_id INT PRIMARY KEY, start_date INT, end_date INT, title_id INT, channel TEXT,
+				subtitle TEXT, count INT)');
+			$this->_sqlite_query('CREATE INDEX prog_date_idx ON program_tbl(start_date)');
+			$this->_sqlite_query('CREATE TABLE title_tbl (title_id INT PRIMARY KEY, title TEXT, season TEXT)');
+			$this->_sqlite_query("INSERT OR REPLACE INTO config_tbl (key, value) VALUES ('dbver', 1)");
+		}
+	}
+
+	function _sqlite_query($sql) {
+		if (!$this->cache_db->exec($sql)) {
+			$this->_err("Failed sql (%d:%s) %s", $this->cache_db->lastErrorCode(), $this->cache_db->lastErrorMsg(), $sql);
+		}
 	}
 }
 
